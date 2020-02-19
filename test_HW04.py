4704fbcb86ca9b436af32d9bab14d88459cb2ce5
chinmliu <chinmliu@126.com>
2020-02-19 15:22:45 -0500

    changes

--- test_HW04.py
+++ test_HW04.py
@@ -9,7 +9,7 @@ class TestResults(unittest.TestCase):
 
     def testOutputList(self):
         self.assertEqual(get_repo("chinmliu"),
-                         [['CS_546', 9], ['git_fall_2019', 2], ['HW02TriangleTest', 9], ['HW04a', 8], ['HW09', 2], ['SSW-567', 2], ['SSW-810', 2]])
+                         [['CS_546', 9], ['git_fall_2019', 2], ['HW02TriangleTest', 9], ['HW04a', 11], ['HW09', 2], ['SSW-567', 2], ['SSW-810', 2]])
 
 
 if __name__ == '__main__':
