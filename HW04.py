ae49bc4153acd51d66fb051734f8813cfc0df8d1
chinmliu <chinmliu@126.com>
2020-02-25 22:39:44 -0500

    Mock

--- HW04.py
+++ HW04.py
@@ -7,7 +7,11 @@ import requests
 def get_repo(id):
     url = "https://api.github.com/users/" + id + "/repos"
     r = requests.get(url)
-    j = r.json()
+    return r.json()
+
+
+def get_commits(id):
+    j = get_repo(id)
     list = []
     for item in j:
         c_url = "https://api.github.com/repos/" + id + "/" + item["name"] + "/commits"
@@ -18,9 +22,8 @@ def get_repo(id):
 
 
 def get_results(id):
-    result = get_repo(id)
+    result = get_commits(id)
     for i in result:
         print("Repo: " + i[0] + " " + "Number of commits: " + str(i[1]))
 
-
-get_results("chinmliu")
+# get_results("chinmliu")
--- test_HW04.py
+++ test_HW04.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python
 # _*_ coding:utf-8 _*_
 
+from unittest import mock
 import unittest
-from HW04 import get_repo
+from unittest.mock import Mock
+
+import HW04
 
 
 class TestResults(unittest.TestCase):
 
-    def testOutputList(self):
-        self.assertEqual(get_repo("chinmliu"),
-                         [['CS_546', 9], ['git_fall_2019', 2], ['HW02TriangleTest', 9], ['HW04a', 11], ['HW09', 2], ['SSW-567', 2], ['SSW-810', 2]])
+    @mock.patch("HW04.requests.get")
+    def testGetRepo(self, mock_get_repo):
+        r_results = [{'name': 'CS-546'}, {'name': 'HW02TriangleTest'}, {'name': 'HW04a'}, {'name': 'HW09'},
+                     {'name': 'SSW-567'}]
+        mock_get_repo.return_value.json.return_value = r_results
+        self.assertEqual(r_results, HW04.get_repo('chinmliu'))
+
+    @mock.patch("HW04.requests.get")
+    @mock.patch("HW04.get_repo")
+    def testGetCommits(self, mock_get_repo, mock_get_commits):
+        mock_get_repo.return_value = [{'name': 'CS-546'}, {'name': 'HW02TriangleTest'}, {'name': 'HW04a'}, {'name': 'HW09'},
+                     {'name': 'SSW-567'}]
+        raw_list = [['CS-546', 1], ['HW02TriangleTest', 1], ['HW04a', 1], ['HW09', 1], ['SSW-567', 1]]
+        mock_get_commits.return_value.json.return_value = {1, 1, 1, 1, 1}
+        self.assertEqual(raw_list, HW04.get_commits('chinmliu'))
 
 
 if __name__ == '__main__':
